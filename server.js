8f2abf917e4c9c97e22dc93494db1eda1397227b
Vojnuk <martel.gilgames@gmail.com>
2019-07-11 22:37:50 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+'esversion:6';
+
+const MongoClient = require("mongodb");
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const port = 8000;
+
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+MongoClient.connect("mongodb://localhost:20000,localhost:20001,localhost:20002/?replicaSet=mapa", { useNewUrlParser: true, poolSize: 50, })
+.then(client => {
+    const db = client.db('settlements');
+    const collection = db.collection('cities');
+    app.locals.collection = collection;
+    app.listen(port, () => console.info(`REST API running on port ${port}`));
+  }).catch(error => console.error(error));
+
+
+app.get('/',cors(), (req, res) => {
+    const collection = req.app.locals.collection;
+    collection.find({}).project({"_id":0}).toArray()
+        .then(response => res.status(200).json(response))
+        .catch(error => console.error(error));
+});
+
+
+app.get('/pictures/tvrdjava.jpg', (req, res) => {
+         res.sendFile('/home/vojnuk/Desktop/projects/mapa/pictures/tvrdjava.jpg');
+         });
+
+
+
